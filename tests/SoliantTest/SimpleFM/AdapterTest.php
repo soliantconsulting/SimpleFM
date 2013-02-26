c7bf4ba04cda780ae86192ba5bcb8ca32c4b2928
vinayganesh <vinayganeshh@gmail.com>
2013-02-25 18:53:31 -0800

    Remaining Unit test cases

--- tests/SoliantTest/SimpleFM/AdapterTest.php
+++ tests/SoliantTest/SimpleFM/AdapterTest.php
@@ -49,16 +49,14 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
     public function testSetHostParams()
     {
         $params=array('hostname'=>'127.0.0.1','dbname'=>'testdb','username'=>'root','password'=>'soliant');
-        
         $value = $this->object->setHostParams($params);
         $this->assertTrue($value instanceof $this->object);
         //echo "<pre>";
         //var_dump($this->object);
         //die;
         $this->assertEquals($this->object->getHostname(),'127.0.0.1');
-       
-        //$this->assertEquals($this->object->gethHostParams(),$params);
-              
+        $this->assertEquals($this->object->getDbname(),'testdb');
+        $this->assertEquals($this->object->getUsername(),'root');
     }
 
     /**
@@ -67,10 +65,10 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testSetCredentials()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $params=array('username'=> 'root' , 'password'=>'soliant');
+        $value = $this->object->setCredentials($params);
+        $this->assertTrue($value instanceof $this->object);
+        $this->assertEquals($this->object->getUsername(),'root');           
     }
 
     /**
@@ -79,10 +77,11 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testSetCallParams()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $params=array('layoutname'=> 'tab' , 'commandstring'=>'soliant=consulting');
+        $value = $this->object->setCallParams($params);
+        $this->assertTrue($value instanceof $this->object);
+        $this->assertEquals($this->object->getLayoutname(),'tab'); 
+        $this->assertEquals($this->object->getCommandstring(),'soliant=consulting');
     }
 
     /**
@@ -126,10 +125,10 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testSetUsername()
     {
-    	//$test = $this->object->setUsername('unittest');
-    	//$this->assertTrue($test instanceof $this->object);    	
-    	//assert $this->object->setUserName('unittest') is type Adapter;
-    	//assert $this->object->getUserName() == 'unittest';          
+    	// Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
     }
 
     /**
@@ -138,10 +137,9 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testSetPassword()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $value = $this->object->setPassword('soliant');
+        $this->assertTrue($value instanceof $this->object);
+        //$this->assertEquals(
     }
 
     /**
@@ -173,8 +171,8 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetLayoutname()
     {
-        $test = $this->object->setLayoutname('tab');
-        $this->assertTrue($test instanceof $this->object);
+        $value = $this->object->setLayoutname('tab');
+        $this->assertTrue($value instanceof $this->object);
         $this->assertEquals($this->object->getLayoutname(),'tab'); 
     }
 
@@ -196,10 +194,9 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetCommandstring()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $value = $this->object->setCommandstring('A=B&C=D&E=F');
+        $this->assertTrue($value instanceof $this->object);
+        $this->assertEquals($this->object->getCommandstring(),'A=B&C=D&E=F'); 
     }
 
     /**
@@ -208,10 +205,12 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetCommandarray()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $value = $this->object->setCommandstring('A=B&C=D');
+        $this->assertTrue($value instanceof $this->object);
+        $arr = $this->object->getCommandarray(); 
+        $arr1=array('A' => 'B',
+        			'C' => 'D');      
+        $this->assertEquals($arr, $arr1);
     }
 
     /**
@@ -360,11 +359,269 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $url = '/usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/projectsampledata.xml';
+        $xml = simplexml_load_file($url);
+        $sfmresult = array (
+            'url' => 'http://Mark:[...]@127.0.0.1/fmi/xml/fmresultset.xml?-db=test&-lay=tab&A=B&C=D&E=F',
+            'error'     => null,
+            'errortext' => 'file_get_contents(https://127.0.0.1:8080/fmi/xml/fmresultset.xml): failed to open stream: Connection refused',
+            'errortype' => 'PHP',
+            'count'     => null,
+            'fetchsize' => null,
+            'rows'      => null
+            );
+        
+        $this->object->setUsername('Mark');
+        $this->object->setPassword('soliant');
+        $this->object->setDbname('test');
+        $this->object->setLayoutname('tab');
+        $this->object->setHostname('127.0.0.1');
+        $this->object->setPort('8080');
+        $this->object->setCommandstring('A=B&C=D&E=F');
+        $this->object->setProtocol('https');
+        $this->object->setFmpxmllayoutUri('./abc/fmlayout.xml');
+        
+        $this->assertEquals($this->object->execute(),$sfmresult);
+             
     }
+    
+    public function testParseResult()
+    {
+        $url = '/usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/projectsampledata.xml';
+        $xml = simplexml_load_file($url);
+        //var_dump($xml);
+        $arr = array(
+        	     0 => array (
+        			'index' => 0,
+        			'recid' => 7676,
+       				'modid' => 11,
+        			'PROJECT ID MATCH FIELD' => '1',
+        			'Created By' => 'Tim Thomson',
+        			'Creation TimeStamp' => '02/22/2012 17:19:47',
+        			'Project Name' => 'Launch web site 9',
+        			'Description' => 'myDescription',
+        			'Status' => '4',
+        			'Status on Screen' => 'Overdue',
+        			'Start Date' => '04/13/2011',
+        			'Due Date' => '05/02/2012',
+        			'Days Remaining' => '0',
+       				'Days Elapsed' => '275',
+        			'Project Completion' => '.48',
+        			'Tag' => 'myTag',
+        			'Start Date Project Completion' => '04/13/2011',
+        			'Due Date Project Completion' => '05/02/2012',       				
+        				'Tasks' => array (
+            				'parentindex' => 0,
+            				'parentrecid' => 7676,
+            				'portalindex' => 0,
+           				    'portalrecordcount' => 5,
+            					'rows' => array (
+									0 => array (
+                   				  	 	'index' => 0,
+                  						'modid' => 0,
+                   						'recid' => 14999,
+                   						'Task Name' => 'Site map sketch',
+                   						'TASK ID MATCH FIELD' => '2'),
+                   					
+                					1 => array (
+                						'index' => 1,
+                    					'modid' => 0,
+                   						'recid' => 15000    ,                
+                   						'Task Name' => 'Send art to vendor',
+                  						'TASK ID MATCH FIELD' => '4'),
+                  						
+                					2 => array (
+                						'index' => 2,
+                    					'modid' => 0,
+                    					'recid' => 15001,
+                   						'Task Name' => 'Review mock ups',
+                    					'TASK ID MATCH FIELD' => '5'),
+                					3 => array (
+                						'index' => 3,
+                    					'modid' => 0,
+                    					'recid' => 15002,
+                   						'Task Name' => 'Write page text',
+                   						'TASK ID MATCH FIELD' => '6'),
+
+               						4 => array (
+               						'index' => 4,
+                    				'modid' => 0,
+                    				'recid' => 15003,
+                   					'Task Name' => 'New logo art',
+                   					'TASK ID MATCH FIELD' => '3')
+               						
+               						)
+            			         )
+        			),
+        	    
+        	     1 => array(
+        		    'index' => 1,
+        			'recid' => 7677,
+        			'modid' => 2,
+       				'PROJECT ID MATCH FIELD' => '7',
+       				'Created By' => 'Tim Thomson',
+       				'Creation TimeStamp' => '02/22/2012 17:19:47',
+       				'Project Name' => 'Prototype',
+        		    'Description' => 'Build a working prototype of the new product.',
+        			'Status' => '',
+        			'Status on Screen' => 'Completed',
+        			'Start Date' => '',
+        			'Due Date' => '',
+        			'Days Remaining' => '0',
+        		    'Days Elapsed' => '0',
+        			'Project Completion' => '',
+        			'Tag' => 'manufacturing',
+        			'Start Date Project Completion' => '',
+        			'Due Date Project Completion' => '',
+        			'Tasks' => array (
+            				'parentindex' => 1,
+            				'parentrecid' => 7677,
+            				'portalindex' => 0,
+           				    'portalrecordcount' => 5)),
+        		    
+                 2 => array(
+        		    'index' => 2,
+        			'recid' => 7678,
+       				'modid' => 2,
+        			'PROJECT ID MATCH FIELD' => '13',
+        			'Created By' => 'Tim Thomson',
+        			'Creation TimeStamp' => '02/22/2012 17:19:47',
+        			'Project Name' => 'Investor meeting',
+        			'Description' => 'This is important.  We need the investors to have confidence.',
+        			'Status' => '4',
+        			'Status on Screen' => 'Overdue',
+        			'Start Date' => '12/12/2011',
+        			'Due Date' => '03/22/2012',
+        			'Days Remaining' => '0',
+       				'Days Elapsed' => '73',
+        			'Project Completion' => '.4285714285714286',
+        			'Tag' => 'finance',
+        			'Start Date Project Completion' => '01/02/2012',
+        			'Due Date Project Completion' => '03/22/2012',       				
+        				'Tasks' => array (
+            				'parentindex' => 2,
+            				'parentrecid' => 7678,
+            				'portalindex' => 0,
+           				    'portalrecordcount' => 5,
+            					'rows' => array (
+									0 => array (
+                   				  	 	'index' => 0,
+                  						'modid' => 0,
+                   						'recid' => 15004,
+                   						'Task Name' => 'Gather requirements',
+                   						'TASK ID MATCH FIELD' => '1'),
+                   					
+                					1 => array (
+                						'index' => 1,
+                    					'modid' => 0,
+                   						'recid' => 15008    ,                
+                   						'Task Name' => 'Investor meeting',
+                  						'TASK ID MATCH FIELD' => '13'),
+                  						
+                					2 => array (
+                						'index' => 2,
+                    					'modid' => 0,
+                    					'recid' => 15009,
+                   						'Task Name' => 'Final draft of slides',
+                    					'TASK ID MATCH FIELD' => '12'),
+                    					
+                					3 => array (
+                						'index' => 3,
+                    					'modid' => 0,
+                    					'recid' => 15010,
+                   						'Task Name' => 'Complete business plan',
+                   						'TASK ID MATCH FIELD' => '10'),
+
+               						4 => array (
+               							'index' => 4,
+                    					'modid' => 0,
+                    					'recid' => 15011,
+                   						'Task Name' => 'First draft of slides',
+                   						'TASK ID MATCH FIELD' => '11'),
+                   						
+                   					5 => array (
+               							'index' => 5,
+                    					'modid' => 0,
+                    					'recid' => 15012,
+                   						'Task Name' => 'Market research',
+                   						'TASK ID MATCH FIELD' => '14'),
+                   						
+                   					6 => array (
+               							'index' => 6,
+                    					'modid' => 0,
+                    					'recid' => 15013,
+                   						'Task Name' => 'Competitive analysis',
+                   						'TASK ID MATCH FIELD' => '15')
+               						
+               						)
+            			         )
+            			         ),
+            		          			         
+        	     3 => array(
+        		    'index' => 3,
+        			'recid' => 7707,
+        			'modid' => 0,
+        			'PROJECT ID MATCH FIELD' => '10',
+        			'Created By' => 'Admin',
+        			'Creation TimeStamp' => '08/15/2012 23:42:13',
+        			'Project Name' => 'myNewProjectName',
+        			'Description' => 'myDescription',
+        			'Status' => '',
+        			'Status on Screen' => 'Completed',
+        			'Start Date' => '',
+        			'Due Date' => '',
+        			'Days Remaining' => '0',
+        			'Days Elapsed' => '0',
+        			'Project Completion' => '',
+       			 	'Tag' => 'myTag',
+       			 	'Start Date Project Completion' => '',
+       			 	'Due Date Project Completion' => '' ,   				
+        				'Tasks' => array (
+            				'parentindex' => 3,
+            				'parentrecid' => 7707,
+            				'portalindex' => 0,
+           				    'portalrecordcount' => 5,
+            					       						
+                        			         )
+            			         ),
+        	     
+        	     4 => array(
+        		    'index' => 4,
+        			'recid' => 7708,
+        			'modid' => 1,
+        			'PROJECT ID MATCH FIELD' => '11',
+        			'Created By' => 'Admin',
+        			'Creation TimeStamp' => '08/16/2012 00:22:48',
+        			'Project Name' => 'myNewestProjectName',
+        			'Description' => 'myDescription',
+        			'Status' => '4',
+        			'Status on Screen' => 'Overdue',
+        			'Start Date' => '08/16/2012',
+        			'Due Date' => '08/17/2012',
+        			'Days Remaining' => '0',
+        			'Days Elapsed' => '1',
+        			'Project Completion' => '.2',
+        			'Tag' => 'myTag',
+        			'Start Date Project Completion' => '08/16/2012',
+        			'Due Date Project Completion' => '08/17/2012'   ,				
+        				'Tasks' => array (
+            				'parentindex' => 4,
+            				'parentrecid' => 7708,
+            				'portalindex' => 0,
+           				    'portalrecordcount' => 5,
+            					'rows' => array (
+            					 0 => array (
+                   				  	 	'index' => 0,
+                  						'modid' => 2,
+                   						'recid' => 15099,
+                   						'Task Name' => 'Zoink',
+                   						'TASK ID MATCH FIELD' => '18'),
+               						
+               						   ))),); 						     			         
+        		        		    
+    	$this->assertEquals($this->object->parseResult($xml),$arr);  	
+    }
+    
 
     /**
      * @covers Soliant\SimpleFM\Adapter::displayXmlError
@@ -372,22 +629,36 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testDisplayXmlError()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
+        $url= '/usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/sample.xml';
+        $xml = simplexml_load_file($url);		
+        $errors = libxml_get_errors();
+		//$error = XML_ERR_TAG_NAME_MISMATCH;
+        var_dump($errors);
+        $string = '
+----------------------------------------------^
+Fatal Error 76: Opening and ending tag mismatch: titles line 4 and title
+  Line: 4
+  Column: 46
+  File: /usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/sample.xml
 
+--------------------------------------------
+
+';
+        foreach ($errors as $error) {
+			$this->assertEquals($this->object->displayXmlError($error,$xml),$string);	
+		}
+      }  
+        
+    
     /**
      * @covers Soliant\SimpleFM\Adapter::extractErrorFromPhpMessage
      * @todo   Implement testExtractErrorFromPhpMessage().
      */
     public function testExtractErrorFromPhpMessage()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+    	$return = array('error' => '401' , 'errortext' => 'Unauthorized' , 'errortype' => 'HTTP');
+        $string = 'HTTP/1.1 401 Unauthorized';
+        $this->assertEquals($this->object->extractErrorFromPhpMessage($string) , $return);
     }
 
     /**
@@ -396,10 +667,7 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testErrorToEnglish()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-        
+    	$error = array( 0 => 'No Error', 10 => 'Requested data is missing');
+        $this->assertEquals($this->object->errorToEnglish(10),'Requested data is missing');    
     }
 }

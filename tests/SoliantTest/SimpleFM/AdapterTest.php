ecb95d4440b862be90d5590b8ff8c22424dcd46b
vinayganesh <vinayganeshh@gmail.com>
2013-02-27 15:50:04 -0800

    Added the relative path to the directory and Changed the test cases  for ParseResult

--- tests/SoliantTest/SimpleFM/AdapterTest.php
+++ tests/SoliantTest/SimpleFM/AdapterTest.php
@@ -1,4 +1,5 @@
 <?php
+ 
 /**
  * This source file is subject to the MIT license that is bundled with this package in the file LICENSE.txt.
  *
@@ -19,15 +20,16 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
     /**
      * @var Adapter
      */
-    protected $object;
+    protected $object;  
 
     /**
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
     protected function setUp()
-    {
-        $this->object = new Adapter();   
+    {    	
+    	
+    	$this->object = new Adapter();   
     }
 
     /**
@@ -354,8 +356,8 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
-        $url = '/usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/projectsampledata.xml';
-        $xml = simplexml_load_file($url);
+    	$file = dirname(__FILE__) . '/TestAssets/projectsampledata.xml';
+        $xml = simplexml_load_file($file);
         $sfmresult = array (
             'url' => 'http://Mark:[...]@127.0.0.1/fmi/xml/fmresultset.xml?-db=test&-lay=tab&A=B&C=D&E=F',
             'error'     => null,
@@ -375,246 +377,50 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
         $this->object->setCommandstring('A=B&C=D&E=F');
         $this->object->setProtocol('https');
         $this->object->setFmpxmllayoutUri('./abc/fmlayout.xml');
-        
         $this->assertEquals($this->object->execute(),$sfmresult);
              
     }
     
     public function testParseResult()
     {
-        $url = '/usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/projectsampledata.xml';
-        $xml = simplexml_load_file($url);
-        //var_dump($xml);
-        $arr = array(
-        	     0 => array (
-        			'index' => 0,
-        			'recid' => 7676,
-       				'modid' => 11,
-        			'PROJECT ID MATCH FIELD' => '1',
-        			'Created By' => 'Tim Thomson',
-        			'Creation TimeStamp' => '02/22/2012 17:19:47',
-        			'Project Name' => 'Launch web site 9',
-        			'Description' => 'myDescription',
-        			'Status' => '4',
-        			'Status on Screen' => 'Overdue',
-        			'Start Date' => '04/13/2011',
-        			'Due Date' => '05/02/2012',
-        			'Days Remaining' => '0',
-       				'Days Elapsed' => '275',
-        			'Project Completion' => '.48',
-        			'Tag' => 'myTag',
-        			'Start Date Project Completion' => '04/13/2011',
-        			'Due Date Project Completion' => '05/02/2012',       				
-        				'Tasks' => array (
-            				'parentindex' => 0,
-            				'parentrecid' => 7676,
-            				'portalindex' => 0,
-           				    'portalrecordcount' => 5,
-            					'rows' => array (
-									0 => array (
-                   				  	 	'index' => 0,
-                  						'modid' => 0,
-                   						'recid' => 14999,
-                   						'Task Name' => 'Site map sketch',
-                   						'TASK ID MATCH FIELD' => '2'),
-                   					
-                					1 => array (
-                						'index' => 1,
-                    					'modid' => 0,
-                   						'recid' => 15000    ,                
-                   						'Task Name' => 'Send art to vendor',
-                  						'TASK ID MATCH FIELD' => '4'),
-                  						
-                					2 => array (
-                						'index' => 2,
-                    					'modid' => 0,
-                    					'recid' => 15001,
-                   						'Task Name' => 'Review mock ups',
-                    					'TASK ID MATCH FIELD' => '5'),
-                					3 => array (
-                						'index' => 3,
-                    					'modid' => 0,
-                    					'recid' => 15002,
-                   						'Task Name' => 'Write page text',
-                   						'TASK ID MATCH FIELD' => '6'),
-
-               						4 => array (
-               						'index' => 4,
-                    				'modid' => 0,
-                    				'recid' => 15003,
-                   					'Task Name' => 'New logo art',
-                   					'TASK ID MATCH FIELD' => '3')
-               						
-               						)
-            			         )
-        			),
-        	    
-        	     1 => array(
-        		    'index' => 1,
-        			'recid' => 7677,
-        			'modid' => 2,
-       				'PROJECT ID MATCH FIELD' => '7',
-       				'Created By' => 'Tim Thomson',
-       				'Creation TimeStamp' => '02/22/2012 17:19:47',
-       				'Project Name' => 'Prototype',
-        		    'Description' => 'Build a working prototype of the new product.',
-        			'Status' => '',
-        			'Status on Screen' => 'Completed',
-        			'Start Date' => '',
-        			'Due Date' => '',
-        			'Days Remaining' => '0',
-        		    'Days Elapsed' => '0',
-        			'Project Completion' => '',
-        			'Tag' => 'manufacturing',
-        			'Start Date Project Completion' => '',
-        			'Due Date Project Completion' => '',
-        			'Tasks' => array (
-            				'parentindex' => 1,
-            				'parentrecid' => 7677,
-            				'portalindex' => 0,
-           				    'portalrecordcount' => 5)),
-        		    
-                 2 => array(
-        		    'index' => 2,
-        			'recid' => 7678,
-       				'modid' => 2,
-        			'PROJECT ID MATCH FIELD' => '13',
-        			'Created By' => 'Tim Thomson',
-        			'Creation TimeStamp' => '02/22/2012 17:19:47',
-        			'Project Name' => 'Investor meeting',
-        			'Description' => 'This is important.  We need the investors to have confidence.',
-        			'Status' => '4',
-        			'Status on Screen' => 'Overdue',
-        			'Start Date' => '12/12/2011',
-        			'Due Date' => '03/22/2012',
-        			'Days Remaining' => '0',
-       				'Days Elapsed' => '73',
-        			'Project Completion' => '.4285714285714286',
-        			'Tag' => 'finance',
-        			'Start Date Project Completion' => '01/02/2012',
-        			'Due Date Project Completion' => '03/22/2012',       				
-        				'Tasks' => array (
-            				'parentindex' => 2,
-            				'parentrecid' => 7678,
-            				'portalindex' => 0,
-           				    'portalrecordcount' => 5,
-            					'rows' => array (
-									0 => array (
-                   				  	 	'index' => 0,
-                  						'modid' => 0,
-                   						'recid' => 15004,
-                   						'Task Name' => 'Gather requirements',
-                   						'TASK ID MATCH FIELD' => '1'),
-                   					
-                					1 => array (
-                						'index' => 1,
-                    					'modid' => 0,
-                   						'recid' => 15008    ,                
-                   						'Task Name' => 'Investor meeting',
-                  						'TASK ID MATCH FIELD' => '13'),
-                  						
-                					2 => array (
-                						'index' => 2,
-                    					'modid' => 0,
-                    					'recid' => 15009,
-                   						'Task Name' => 'Final draft of slides',
-                    					'TASK ID MATCH FIELD' => '12'),
-                    					
-                					3 => array (
-                						'index' => 3,
-                    					'modid' => 0,
-                    					'recid' => 15010,
-                   						'Task Name' => 'Complete business plan',
-                   						'TASK ID MATCH FIELD' => '10'),
-
-               						4 => array (
-               							'index' => 4,
-                    					'modid' => 0,
-                    					'recid' => 15011,
-                   						'Task Name' => 'First draft of slides',
-                   						'TASK ID MATCH FIELD' => '11'),
-                   						
-                   					5 => array (
-               							'index' => 5,
-                    					'modid' => 0,
-                    					'recid' => 15012,
-                   						'Task Name' => 'Market research',
-                   						'TASK ID MATCH FIELD' => '14'),
-                   						
-                   					6 => array (
-               							'index' => 6,
-                    					'modid' => 0,
-                    					'recid' => 15013,
-                   						'Task Name' => 'Competitive analysis',
-                   						'TASK ID MATCH FIELD' => '15')
-               						
-               						)
-            			         )
-            			         ),
-            		          			         
-        	     3 => array(
-        		    'index' => 3,
-        			'recid' => 7707,
-        			'modid' => 0,
-        			'PROJECT ID MATCH FIELD' => '10',
-        			'Created By' => 'Admin',
-        			'Creation TimeStamp' => '08/15/2012 23:42:13',
-        			'Project Name' => 'myNewProjectName',
-        			'Description' => 'myDescription',
-        			'Status' => '',
-        			'Status on Screen' => 'Completed',
-        			'Start Date' => '',
-        			'Due Date' => '',
-        			'Days Remaining' => '0',
-        			'Days Elapsed' => '0',
-        			'Project Completion' => '',
-       			 	'Tag' => 'myTag',
-       			 	'Start Date Project Completion' => '',
-       			 	'Due Date Project Completion' => '' ,   				
-        				'Tasks' => array (
-            				'parentindex' => 3,
-            				'parentrecid' => 7707,
-            				'portalindex' => 0,
-           				    'portalrecordcount' => 5,
-            					       						
-                        			         )
-            			         ),
-        	     
-        	     4 => array(
-        		    'index' => 4,
-        			'recid' => 7708,
-        			'modid' => 1,
-        			'PROJECT ID MATCH FIELD' => '11',
-        			'Created By' => 'Admin',
-        			'Creation TimeStamp' => '08/16/2012 00:22:48',
-        			'Project Name' => 'myNewestProjectName',
-        			'Description' => 'myDescription',
-        			'Status' => '4',
-        			'Status on Screen' => 'Overdue',
-        			'Start Date' => '08/16/2012',
-        			'Due Date' => '08/17/2012',
-        			'Days Remaining' => '0',
-        			'Days Elapsed' => '1',
-        			'Project Completion' => '.2',
-        			'Tag' => 'myTag',
-        			'Start Date Project Completion' => '08/16/2012',
-        			'Due Date Project Completion' => '08/17/2012'   ,				
-        				'Tasks' => array (
-            				'parentindex' => 4,
-            				'parentrecid' => 7708,
-            				'portalindex' => 0,
-           				    'portalrecordcount' => 5,
-            					'rows' => array (
-            					 0 => array (
-                   				  	 	'index' => 0,
-                  						'modid' => 2,
-                   						'recid' => 15099,
-                   						'Task Name' => 'Zoink',
-                   						'TASK ID MATCH FIELD' => '18'),
-               						
-               						   ))),); 						     			         
-        		        		    
-    	$this->assertEquals($this->object->parseResult($xml),$arr);  	
+        $file = dirname(__FILE__) . '/TestAssets/projectsampledata.xml';
+        $xml = simplexml_load_file($file);
+        
+        
+        $this->object->setHostParams(
+    	array(
+        'hostname' => 'shn.serveftp.net',
+        'dbname'   => 'FMServer_Sample',
+        'username' => 'Admin',
+        'password' => '' 
+    		)
+		);
+		
+		$this->object->setCallParams(
+    	array(
+        'layoutname'    => 'Tasks',
+        'commandstring' => '-max=10&-skip=5&-findall'
+    		)
+		);
+		
+		$this->object->setLayoutname('Projects');
+		$this->object->setCommandstring('-findall');
+        
+        //rowsbyrecid as TRUE
+        $this->object->setRowsbyrecid(TRUE);	
+        $result = $this->object->execute();		 
+       	$rowset = $result['rows'];
+       	$rows =$rowset[7676]['Tasks']['rows'][15001]['Task Name'];       
+        $this->assertEquals($rows, 'Review mock ups');
+        
+        
+        //rowsbyrecid as FALSE 
+        $this->object->setRowsbyrecid(FALSE);	
+        $result = $this->object->execute();		       
+       	$rowset = $result['rows'];
+       	$rows =$rowset[4]['Tasks']['rows'][0]['Task Name'];
+        $this->assertEquals($rows, 'Zoink1');     			         	        		    
+    	  	
     }
     
 
@@ -624,11 +430,9 @@ class AdapterTest extends \PHPUnit_Framework_TestCase
      */
     public function testDisplayXmlError()
     {
-        $url= '/usr/local/zend/apache2/htdocs/SimpleFM/tests/SoliantTest/SimpleFM/TestAssets/sample.xml';
-        $xml = simplexml_load_file($url);		
+        $file = dirname(__FILE__) . '/TestAssets/sample.xml';
+        $xml = simplexml_load_file($file);		
         $errors = libxml_get_errors();
-		//$error = XML_ERR_TAG_NAME_MISMATCH;
-        var_dump($errors);
         $string = '
 ----------------------------------------------^
 Fatal Error 76: Opening and ending tag mismatch: titles line 4 and title
@@ -662,6 +466,7 @@ Fatal Error 76: Opening and ending tag mismatch: titles line 4 and title
      */
     public function testErrorToEnglish()
     {
+    	
     	$error = array( 0 => 'No Error', 10 => 'Requested data is missing');
         $this->assertEquals($this->object->errorToEnglish(10),'Requested data is missing');    
     }

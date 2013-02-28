f371447d592f4392d8eee77c25182256d3e21940
Jeremiah Small <me@jsmall.us>
2013-02-27 23:17:18 -0800

    Add Curl, FileGetContents and Mock loaders

---
+++ library/Soliant/SimpleFM/Loader/Mock.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENSE.txt.
+ *
+ * @package   Soliant\SimpleFM\ZF2
+ * @copyright Copyright (c) 2007-2013 Soliant Consulting, Inc. (http://www.soliantconsulting.com)
+ * @author    jsmall@soliantconsulting.com
+ */
+
+namespace Soliant\SimpleFM\Loader;
+
+require_once('AbstractLoader.php');
+
+use Soliant\SimpleFM\Loader\AbstractLoader;
+use Soliant\SimpleFM\Adapter;
+
+class Mock extends AbstractLoader
+{
+    /**
+     * @var string
+     */
+    protected $testXml;
+    
+    /**
+     * @return the $testXml
+     */
+    public function getTestXml ()
+    {
+        return $this->testXml;
+    }
+
+	/**
+     * @param string $testXml
+     */
+    public function setTestXml ($testXml)
+    {
+        $this->testXml = $testXml;
+        return $this;
+    }
+
+	/**
+     * @return SimpleXMLElement
+     */
+    public function load(Adapter $adapter, $testXmlOverride=NULL)
+    {
+        $this->adapter = $adapter;
+        
+        $testXml = $testXmlOverride ? $testXmlOverride : $this->testXml;
+        
+        self::prepare();
+        
+        libxml_use_internal_errors(true);
+        
+        return simplexml_load_string($testXml);
+    
+    }
+    
+}

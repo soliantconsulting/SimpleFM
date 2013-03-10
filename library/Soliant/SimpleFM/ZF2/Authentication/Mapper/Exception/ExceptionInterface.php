6fee8c870aef6be31e61b0128a6e3818ac665c45
Jeremiah Small <me@jsmall.us>
2013-03-09 16:38:05 -0800

    Housekeeping comments in custom exception classes.

--- library/Soliant/SimpleFM/ZF2/Authentication/Mapper/Exception/ExceptionInterface.php
+++ library/Soliant/SimpleFM/ZF2/Authentication/Mapper/Exception/ExceptionInterface.php
@@ -2,11 +2,5 @@
 
 namespace Soliant\SimpleFM\ZF2\Authentication\Mapper\Exception;
 
-/**
- * @uses       Soliant\SimpleFM\ZF2\Authentication\Mapper\Exception
- * @category   Apple, Inc.
- * @package    Soliant\SimpleFM\ZF2\Authentication
- * @subpackage Adapter\Exception
- */
 interface ExceptionInterface extends \Zend\Authentication\Adapter\Exception\ExceptionInterface
 {}
---
+++ library/Soliant/SimpleFM/ZF2/Authentication/Storage/Exception/InvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Soliant\SimpleFM\ZF2\Authentication\Storage\Exception;
+
+class InvalidArgumentException
+    extends \InvalidArgumentException
+    implements ExceptionInterface
+{
+} 
